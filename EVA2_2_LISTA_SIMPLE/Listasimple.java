ed5f5066c371b1e94f219ca1262fd0be588d230b
Angel Moreno <cacn20.20@gmail.com>
2026-09-23 17:58:25 -0600

    practica2_avance_3

--- EVA2_2_LISTA_SIMPLE/Listasimple.java
+++ EVA2_2_LISTA_SIMPLE/Listasimple.java
@@ -3,10 +3,12 @@ package EVA2_2_LISTA_SIMPLE;
 import EVA2_2_LISTA_SIMPLE.Nodo.Nodos;
 
 public class Listasimple {
-    private Nodos    inicio;
+    private Nodos inicio;
+    private Nodos fin;
 
     public Listasimple(){
         inicio = null; //al crear la lista se crea vacia
+        fin = null;
     }
     // Siempre se agrefan al final de la lista 
     public void agregar(int valor){
@@ -14,13 +16,14 @@ public class Listasimple {
         //y luego ??
         if (inicio == null){ //lista vacia?
             inicio = nuevo;
-
+            fin = nuevo;
         } else{
-            Nodos   temp = inicio;
+            /*Nodos   temp = inicio;
             while (temp.getSiguiente() != null) { 
-                temp = temp.getSiguiente();
-            }
-            temp.setSiguiente(nuevo);// concto de nuevo al final de la lista
+                temp = temp.getSiguiente();*/ // ya no necesitamos recorrer la lista, ya tenemos el fin
+            Nodos temp = fin; // ya tenemos el fin, no necesitamos recorrer la lista
+            temp.setSiguiente(nuevo);
+            fin = nuevo;
         }
     }
     public void imprimir(){
--- EVA2_2_LISTA_SIMPLE/main.java
+++ EVA2_2_LISTA_SIMPLE/main.java
@@ -2,12 +2,14 @@ package EVA2_2_LISTA_SIMPLE;
 public class main {
     public static void main(String[] args) {
         Listasimple lista = new Listasimple();
-        lista.agregar(100);
-        lista.agregar(200);
-        lista.agregar(300);
-        lista.agregar(400);
-        lista.agregar(500);
-            lista.imprimir();
+        long ini, fin, tiempo;
+        ini = System.nanoTime(); // Tomamos la hora antes de empezar a ejecutar el código
+        for (int i = 0; i < 1000000; i++) {
+            lista.agregar((int)(Math.random() * 100));
         }
+        fin = System.nanoTime(); // toma la hora después de ejecutar el código
+        tiempo = fin - ini;
+        System.out.println("Tiempo de ejecución: " + tiempo + " ns");
+    }
 }
 
